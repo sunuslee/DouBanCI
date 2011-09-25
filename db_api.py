e2a887d954f63a8abe803384f161740c0fd4946c
sunus lee <sunuslee@gmail.com>
2011-09-25 21:12:10 +0800

    i think there might be some bugs
    i am tring to fix them in <db_api>!

--- db_api.py
+++ db_api.py
@@ -7,7 +7,6 @@ LOCK_UN = fcntl.LOCK_UN
 LOCK_NB = fcntl.LOCK_NB
 LIMIT = 38
 def try_api(api_url):
-        flog = open("log", "a+")
         while True:
                 while True:
                         try:
@@ -15,42 +14,41 @@ def try_api(api_url):
                                 fcntl.flock(fh.fileno(), LOCK_EX|LOCK_NB)
                                 break
                         except IOError:
-                                flog.write("FILE's LOCKED,WAIT....{0}\n".format(time.ctime()))
                                 fh.close()
                                 time.sleep(1)
 
                 if(fh.tell() == 0):
                         data = urllib.request.urlopen(api_url)
+                        fh.write("[LOG] START at {0}\n".format(time.ctime()))
+                        fh.write("[LOG] cnt == {0:2} {1:<24} {2}\n".format(1, time.ctime(), api_url[:-40]))
                         fh.write("{0},{1},{2}\n".format(time.ctime(), 1, int(time.time())))
-                        flog.write("START at {0}\n".format(time.ctime()))
-                        flog.write("cnt == 1\n")
                         break;
-
                 os.lseek(fh.fileno(), -17, os.SEEK_END)
                 lastline = fh.read(16)
                 lasttime, cnt  = lastline.split(',')[-1:-3:-1] # get the last two element, cnt and lasttime 
                 if(int(time.time()) - int(lasttime) >= 65):
                         cnt = 1
                         data = urllib.request.urlopen(api_url)
+                        fh.write("[LOG] START at {0}\n".format(time.ctime()))
+                        fh.write("[LOG] cnt == {0:2} {1:<24} {2}\n".format(1, time.ctime(), api_url[:-40]))
                         fh.write("{0},{1},{2}\n".format(time.ctime(), cnt, int(time.time())))
-                        flog.write("START at {0}\n".format(time.ctime()))
-                        flog.write("cnt == 1\n")
                         break
                 elif int(cnt) >= LIMIT:
-                        flog.write("No More API at this time {0}\n".format(time.ctime()))
-                        flog.write("END at {0}\n".format(time.ctime()))
-                        fcntl.flock(fh.fileno(), LOCK_UN)
+                        fh.write("[LOG] No More API at this time {0}\n".format(time.ctime()))
+                        fh.write("[LOG] END at {0},0,{1}\n".format(time.ctime(), int(time.time())))
+                        #fcntl.flock(fh.fileno(), LOCK_UN) JUST LET THE FILE KEEP ON BLOCKING WHILE IT'S SLEEPING , 25, 9, 2011
+                        #I am not sure whether it will work or not:<
                         fh.close()
                         time.sleep(65)
                         #time.sleep(int(lasttime) + 65 - int(time.time()))
                 else:
                         cnt = int(cnt)
                         cnt += 1
-                        fh.write("{0},{1},{2}\n".format(time.ctime(),cnt, lasttime ))
-                        flog.write("cnt == {0}\n".format(cnt))
+                        fh.write("[LOG] cnt == {0:2} {1:<24} {2}\n".format(cnt, time.ctime(), api_url[:-40]))
+                        fh.write("{0},{1},{2}\n".format(time.ctime(), cnt, lasttime))
                         data = urllib.request.urlopen(api_url)
                         break
+
         fcntl.flock(fh.fileno(), LOCK_UN)
         fh.close()
-        flog.close()
         return data 
